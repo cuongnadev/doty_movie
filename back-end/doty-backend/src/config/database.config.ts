3e937759d3c985815aaf23bf633724103b188599
Cậu Bé Java <154894942+cuongnadev@users.noreply.github.com>
2025-05-18 23:39:31 +0700

    commit #9

--- back-end/doty-backend/src/config/database.config.ts
+++ back-end/doty-backend/src/config/database.config.ts
@@ -1,17 +1,29 @@
-import { registerAs } from "@nestjs/config";
+import { registerAs } from '@nestjs/config';
+import { configDotenv } from 'dotenv';
+configDotenv();
 
 function required(name: string): string {
-    const value = process.env[name];
-    if(!value) {
-        throw new Error(`Missing environment variable: ${name}`);
-    }
-    return value;
+  const value = process.env[name];
+  if (!value) {
+    throw new Error(`Missing environment variable: ${name}`);
+  }
+  return value;
 }
 
-export default registerAs('database', () => ({
+export default registerAs('database', () => {
+  const isProd = process.env.NODE_ENV === 'production';
+
+  if (isProd) {
+    return {
+      url: required('DATABASE_URL'),
+    };
+  }
+
+  return {
     host: required('DB_HOST'),
     port: parseInt(required('DB_PORT'), 10),
     username: required('DB_USERNAME'),
     password: required('DB_PASSWORD'),
     database: required('DB_NAME'),
-}))
+  };
+});
